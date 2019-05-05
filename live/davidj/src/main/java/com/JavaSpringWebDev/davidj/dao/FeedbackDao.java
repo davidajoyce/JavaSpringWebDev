48a60532467e174933df29cdb3597aee2e0c170a
davidajoyce <davidajoyce141@gmail.com>
2019-05-05 17:05:00 +0100

    add the configuration dependencies for database transactions

--- live/davidj/src/main/java/com/JavaSpringWebDev/davidj/dao/FeedbackDao.java
+++ live/davidj/src/main/java/com/JavaSpringWebDev/davidj/dao/FeedbackDao.java
@@ -1,5 +1,7 @@
 package com.JavaSpringWebDev.davidj.dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Component;
 
 import com.JavaSpringWebDev.davidj.entity.Feedback;
@@ -7,9 +9,15 @@ import com.JavaSpringWebDev.davidj.entity.Feedback;
 @Component
 public class FeedbackDao 
 {
-	public void addFeedback(Feedback fb)
+	
+	private SessionFactory sessionFactory;
+	
+	public void addFeedback(Feedback feedback)
 	{
-		System.out.println("in addFeedback" + fb);
+		System.out.println("in addFeedback" + feedback);
+		
+		Session session = sessionFactory.getCurrentSession();
+		session.save(feedback)
 	}
 
 }
