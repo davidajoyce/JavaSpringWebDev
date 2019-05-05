ea2a7a5fd4bc916a1f28cbe171a79b66469d67b1
davidajoyce <davidajoyce141@gmail.com>
2019-05-05 17:31:52 +0100

    add the spring bean, transaction properties and transaction manager

--- live/davidj/src/main/java/com/JavaSpringWebDev/davidj/dao/FeedbackDao.java
+++ live/davidj/src/main/java/com/JavaSpringWebDev/davidj/dao/FeedbackDao.java
@@ -2,6 +2,7 @@ package com.JavaSpringWebDev.davidj.dao;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.JavaSpringWebDev.davidj.entity.Feedback;
@@ -9,7 +10,7 @@ import com.JavaSpringWebDev.davidj.entity.Feedback;
 @Component
 public class FeedbackDao 
 {
-	
+	@Autowired
 	private SessionFactory sessionFactory;
 	
 	public void addFeedback(Feedback feedback)
@@ -17,7 +18,7 @@ public class FeedbackDao
 		System.out.println("in addFeedback" + feedback);
 		
 		Session session = sessionFactory.getCurrentSession();
-		session.save(feedback)
+		session.save(feedback);
 	}
 
 }
