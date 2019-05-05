36d0cc4d837b3031cc2740c9c1503ec76d00862b
davidajoyce <davidajoyce141@gmail.com>
2019-05-05 17:21:37 +0100

    hibernate config connection pool size

--- MySql/javaWebDev.sql
+++ MySql/javaWebDev.sql
@@ -4,3 +4,9 @@ use javaWebDev;
 
 create table feedback (name varchar(30), email varchar(30), message varchar(10000), 
 regarding int, code varchar(10), rating int);
+
+desc feedback;
+
+insert into feedback values ('David', 'dvid@gmail.com', 'good', 3, 'micro101', 4);
+
+select * from feedback;
